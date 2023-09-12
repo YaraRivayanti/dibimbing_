aaf3649e08b3f3ecac1c62205bdb2489a8f68690
GabrielHuga <irulhuga@gmail.com>
2023-09-12 14:05:08 +0700

    add file and solve 2 numbers

---
+++ Assignment_GIT.py
@@ -0,0 +1,18 @@
+# Nomor 1a
+str_val = "balonku ada lima"
+
+str_val = str_val.replace("o","a")
+str_val = str_val.replace("u","a")
+str_val = str_val.replace("i","a")
+str_val = str_val.replace("e","a")
+
+print(str_val)
+print("Fitur baruu")
+
+# Nomor 1b
+word = input("Masukkan kata (lowercase): ")
+word = word.replace("o", "a")
+word = word.replace("i", "a")
+word = word.replace("u", "a")
+word = word.replace("e", "a")
+print("Berubah menjadi: " + word)
